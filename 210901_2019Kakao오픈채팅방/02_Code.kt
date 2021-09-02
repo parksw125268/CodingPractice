8e22b1752e1ad7796166d23d3df23f8544b04f57
psw <parksw125268@koreasoft.co.kr>
2021-09-02 09:21:01 +0900

    210902_2

--- 210901_2019Kakao오픈채팅방/02_Code.kt
+++ 210901_2019Kakao오픈채팅방/02_Code.kt
@@ -1,35 +1,14 @@
-import java.io.BufferedWriter
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.util.StringTokenizer
-
-fun main(){
-    val record = arrayOf<String>("Enter uid1234 Muzi", "Enter uid4567 Prodo", "Change uid1234 Psw", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan")
-
-    val result = Solution().run {
-        solution(record)
-    }
-    result.forEach {
-        println(it)
-    }
-
-}
 class Solution {
     fun solution(record: Array<String>): Array<String> {
         var id_nameMap = mutableMapOf<String, String>()
         var cnt = 0
         record.forEach{
             val sArr : List<String> = it.split(" ")
-            if (sArr.get(0) == "Enter"){
-                cnt++
-                if (id_nameMap.containsKey(sArr.get(1))){//key가 이미 있는지
-                    id_nameMap.put(sArr.get(1),sArr.get(2))//바꿔
-                }else{
-                    id_nameMap.put(sArr.get(1),sArr.get(2))//넣어
+            if (sArr.get(0) == "Enter" || sArr.get(0) == "Change"){
+                if (sArr.get(0) == "Enter") {
+                    cnt++
                 }
-            }else if (sArr.get(0) == "Change"){
-                id_nameMap.put(sArr.get(1),sArr.get(2))//바꿔
+                id_nameMap.put(sArr.get(1),sArr.get(2))//넣어
             }else if (sArr.get(0) == "Leave"){
                 cnt++
             }
@@ -49,4 +28,3 @@ class Solution {
         return answer
     }
 }
-
